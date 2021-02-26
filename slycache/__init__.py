a01b11fbca6e190ff422b6862fb4ae9b59e98bad
Simon Kelly <skelly@dimagi.com>
2021-02-26 21:22:10 +0200

    Bump version: 0.1.0 → 0.1.1

--- slycache/__init__.py
+++ slycache/__init__.py
@@ -1,5 +1,5 @@
 __author__ = """snopoke"""
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 from .exceptions import InvalidCacheError, SlycacheException
 from .interface import CacheInterface, KeyGenerator
